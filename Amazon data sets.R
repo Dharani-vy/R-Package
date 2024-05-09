3269d99b2607d4950d5a1e8e0885f90678792b2d
Yohesh <yoheshbabu@gmail.com>
2024-05-09 16:30:58 +0530

    new branch created

--- Amazon data sets.R
+++ Amazon data sets.R
@@ -32,7 +32,7 @@ server <- function(input, output) {
   generatePieChart <- eventReactive(input$generate_button, {
     category_data <- table(data()$ratings)
     # Custom color palette
-    custom_colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
+    custom_colors <- c("#daa32e", "#ff7f0e", "#2ca02c", "#daa32e", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
     pie_chart <- plot_ly(labels = names(category_data), values = category_data, type = "pie", textinfo = "percent+label", hole = 0.4, marker = list(colors = custom_colors))
     return(pie_chart)
   })
